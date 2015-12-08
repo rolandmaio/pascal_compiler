c6fd00c8a3b48feafd542ff60bd21c3164664206
Roland Maio <rolandmaio38@gmail.com>
2015-12-08 11:40:42 -0500

    - Build file. Note that the path to the Boost library .so files is explicitly   given.

---
+++ build.sh
@@ -0,0 +1,2 @@
+g++ compile.cpp compiler/src/parsetreelogger/* compiler/src/lexer/* compiler/src/parser/* compiler/src/token/* compiler/src/synthesizer/* compiler/src/opcode/* compiler/src/type/* -lm /usr/lib/x86_64-linux-gnu/libboost*\.so -o compile.exe -std=c++11 -g
+g++ emulate.cpp emulator/* -o emulate.exe -std=c++11 -g
